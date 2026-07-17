2be4738a59b51c3592fcb23678537893b88a98fa
Hari Kodikkalithodathil <hari.campnou@gmail.com>
2026-07-17 23:52:00 +0530

    one class created in accounts app models. one to to be created there. one class created in customer app models.

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    dob=models.DateField()
+    email=models.EmailField(unique=True)
+    phone=models.CharField(max_length=15, unique=True)
+    driving_license_no=models.CharField(max_length=15, unique=True)
