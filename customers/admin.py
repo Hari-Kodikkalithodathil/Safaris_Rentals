67f0b2661255950b5c0ace41ce61fa43dc717210
Hari Kodikkalithodathil <hari.campnou@gmail.com>
2026-07-19 19:43:48 +0530

    models connected to admin

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . models import Customer, CustomerAddress
 
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(CustomerAddress)
--- vehicles/admin.py
+++ vehicles/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . models import Vehicle, VehicleImage
 
 # Register your models here.
+
+admin.site.register(Vehicle)
+admin.site.register(VehicleImage)
