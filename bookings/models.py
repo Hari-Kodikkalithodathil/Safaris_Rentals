acf92ca9d179428cbfa08a2675a926e5d7f53c67
Hari Kodikkalithodathil <hari.campnou@gmail.com>
2026-07-22 23:08:03 +0530

    basic booking validations written

--- bookings/models.py
+++ bookings/models.py
@@ -26,6 +26,7 @@ class Booking(models.Model):
     def __str__(self):
         return f'{self.customer.first_name} {self.customer.last_name} : {self.vehicle.name}'
 
+
 '''The class below is for vehicles when thay become unavailable due to service, breakdowns
  etc. It is not about unavailability due to bookings'''
 class BlockedDate(models.Model):
--- bookings/services.py
+++ bookings/services.py
@@ -1,6 +1,15 @@
 from . models import Booking
+from datetime import datetime
+from django.utils import timezone
+from . models import BlockedDate
 
-def check_vehicle_availability(vehicle, pickup_datetime, return_datetime):
+
+
+'''Checking whether a particular vehicle is available on a particular date.
+If a vehicle is avalible the function retuns true else it returns false'''
+
+def vehicle_availability(vehicle, pickup_datetime, return_datetime):
+
     overlapping_bookings=Booking.objects.filter(
         vehicle=vehicle,
         booking_status__in=["CONFIRMED","PENDING"],
@@ -10,5 +19,40 @@ def check_vehicle_availability(vehicle, pickup_datetime, return_datetime):
 
     if overlapping_bookings.exists():
         return False
+    
+    return True
+
+
+
+'''Checking whether proper dates are entered for pickup and return of vehicles'''
+
+def validate_booking_dates(pickup_datetime, return_datetime):
+
+    if pickup_datetime>=return_datetime:
+        raise ValueError("Return date must be after pickup date")
+    
+    if pickup_datetime<timezone.now(): 
+        raise ValueError("Pickup date cannot be in in the past")
+    
+    return True
+
+
+# print(timezone.localtime())
+
+
+
+'''Checking whether a vehicle is not blocked. The function 'vehicle_not_blocked' returns
+a true value if the vehicle is free or a false value if it is blocked'''
+
+def vehicle_not_blocked(vehicle, pickup_date, return_date):
+
+    blocked_periods=BlockedDate.objects.filter(
+        vehicle=vehicle,
+        start_date__lt=return_date.date(),
+        end_date__gt=pickup_date.date()
+    )
+
+    if blocked_periods.exists():
+        return  False
     
     return True
