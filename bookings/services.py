8c06ad2db6fd8900cd00c30014371731c7327390
Hari Kodikkalithodathil <hari.campnou@gmail.com>
2026-08-01 22:58:47 +0530

    rearraged the pricing function to be above create_booking function

--- bookings/services.py
+++ bookings/services.py
@@ -65,37 +65,6 @@ def vehicle_not_blocked(vehicle, pickup_datetime, return_datetime):
 
 
 
-'''The function that creates an object of Booking'''
-
-def create_booking(customer, vehicle, pickup_datetime, return_datetime,
-                   subtotal, tax, deposit_paid):
-    
-    validate_booking_dates(pickup_datetime, return_datetime)
-
-    if not vehicle_not_blocked(vehicle, pickup_datetime, return_datetime):
-        raise ValueError("The vehicle is under maintenance")
-
-    if not vehicle_availability(vehicle, pickup_datetime, return_datetime):
-        raise ValueError("The vehicle is already booked")
-
-    total_price=subtotal+tax
-
-    booking=Booking.objects.create(
-        customer=customer,
-        vehicle=vehicle,
-        pickup_datetime=pickup_datetime,
-        return_datetime=return_datetime,
-        subtotal=subtotal,
-        tax=tax,
-        total_price=total_price,
-        deposit_paid=deposit_paid,
-    )
-
-    return booking
-
-
-
-
 '''Function to calculate the total price of the booking'''
 
 def calculate_booking_price(vehicle, pickup_datetime, return_datetime):
@@ -126,6 +95,37 @@ def calculate_booking_price(vehicle, pickup_datetime, return_datetime):
         "total_price" : total_price,
         "deposit_paid" : deposit_paid,
     }
+
+
+
+
+'''The function that creates an object of Booking'''
+
+def create_booking(customer, vehicle, pickup_datetime, return_datetime,
+                   subtotal, tax, deposit_paid):
+    
+    validate_booking_dates(pickup_datetime, return_datetime)
+
+    if not vehicle_not_blocked(vehicle, pickup_datetime, return_datetime):
+        raise ValueError("The vehicle is under maintenance")
+
+    if not vehicle_availability(vehicle, pickup_datetime, return_datetime):
+        raise ValueError("The vehicle is already booked")
+
+    total_price=subtotal+tax
+
+    booking=Booking.objects.create(
+        customer=customer,
+        vehicle=vehicle,
+        pickup_datetime=pickup_datetime,
+        return_datetime=return_datetime,
+        subtotal=subtotal,
+        tax=tax,
+        total_price=total_price,
+        deposit_paid=deposit_paid,
+    )
+
+    return booking
 
 
 
